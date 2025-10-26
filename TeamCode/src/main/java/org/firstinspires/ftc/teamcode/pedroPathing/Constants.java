6cc13def25f0cbf5a53cc029f4e21fcc2914760d
D4LM <148903901+D4LMwastaken@users.noreply.github.com>
2025-10-26 13:18:22 -0400

    Migrated Pedro Pathing to repo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.pedroPathing;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
@@ -14,20 +14,20 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(0); // TODO: Add mass in kg
-
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
-
-    public static Follower createFollower(HardwareMap hardwareMap) {
-        return new FollowerBuilder(followerConstants, hardwareMap)
-                .pathConstraints(pathConstraints)
-                .mecanumDrivetrain(driveConstants)
-                .build();
-    }
+            // Automatic tuning
+            .forwardZeroPowerAcceleration(-31.423844301948137)
+            .lateralZeroPowerAcceleration(-31.69672815602704)
+            .mass(8.2);
 
     public static MecanumConstants driveConstants = new MecanumConstants()
+            // Automatic Tuning
+            .xVelocity(62.459028108852124)
+            .yVelocity(56.325609582615655)
+            // TeleOp
+            .useBrakeModeInTeleOp(true)
+            // Default for Mecanum
             .maxPower(1)
-            .rightFrontMotorName("rf") // TODO: Make sure all names match
+            .rightFrontMotorName("rf")
             .rightRearMotorName("rr")
             .leftRearMotorName("lr")
             .leftFrontMotorName("lf")
@@ -37,18 +37,21 @@ public class Constants {
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(-5) // TODO: Add offsets
-            .strafePodX(0.5)
+            .forwardPodY(3.0)
+            .strafePodX(-6.5)
             .distanceUnit(DistanceUnit.INCH)
             .hardwareMapName("pinpoint")
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
-            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
-            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
-}
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
 
-/* TODO Items:
-* Add encoder directions
-* Test localizer
-* Have not started with automatic tuners nor manual tuners
-*
- */
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .pinpointLocalizer(localizerConstants)
+                .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .build();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/RedTeleOp.java
@@ -0,0 +1,148 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.HeadingInterpolator;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import java.util.function.Supplier;
+
+@Configurable
+@TeleOp
+public class RedTeleOp extends OpMode {
+    private Follower follower;
+    public static Pose startingPose; //See ExampleAuto to understand how to use this
+    private boolean automatedDrive;
+    private Supplier<PathChain> pathChain;
+    private TelemetryManager telemetryM;
+    private boolean slowMode = false;
+    private double slowModeMultiplier = 0.35;
+    private double topPower = 0.5;
+    private double intakePower = 1;
+
+    private boolean rtRunning;
+    private boolean ltRunning;
+
+    DcMotor lt; // Top two motors
+    DcMotor rt;
+    CRServo intake; // Not real Sevo, is connector for intakePos...
+
+    @Override
+    public void init() {
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startingPose == null ? new Pose() : startingPose); // Takes starting position from default position in auto beginning
+        follower.update();
+        telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
+
+        lt = hardwareMap.get(DcMotor.class, "lt");
+        rt = hardwareMap.get(DcMotor.class, "rt");
+        intake = hardwareMap.get(CRServo.class, "intake");
+
+        lt.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rt.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        lt.setDirection(DcMotorSimple.Direction.REVERSE); // Left reversed or right...
+
+        pathChain = () -> follower.pathBuilder() //Lazy Curve Generation
+                .addPath(new Path(new BezierLine(follower::getPose, new Pose(38, 34))))
+                .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(follower::getHeading, Math.toRadians(90), 0.8))
+                .build();
+    }
+
+    @Override
+    public void start() {
+        //The parameter controls whether the Follower should use break mode on the motors (using it is recommended).
+        //In order to use float mode, add .useBrakeModeInTeleOp(true); to your Drivetrain Constants in Constant.java (for Mecanum)
+        //If you don't pass anything in, it uses the default (false)
+        follower.startTeleopDrive();
+    }
+
+    @Override
+    public void loop() {
+        //Call this once per loop
+        follower.update();
+        telemetryM.update();
+        telemetry.update();
+
+        if (!automatedDrive) {
+            //Make the last parameter false for field-centric
+            //In case the drivers want to use a "slowMode" you can scale the vectors
+
+            //This is the normal version to use in the TeleOp
+            if (!slowMode) follower.setTeleOpDrive(
+                    -gamepad1.left_stick_y,
+                    -gamepad1.left_stick_x,
+                    -gamepad1.right_stick_x,
+                    true // Robot Centric
+            );
+
+                //This is how it looks with slowMode on
+            else follower.setTeleOpDrive(
+                    -gamepad1.left_stick_y * slowModeMultiplier,
+                    -gamepad1.left_stick_x * slowModeMultiplier,
+                    -gamepad1.right_stick_x * slowModeMultiplier,
+                    true // Robot Centric
+            );
+        }
+
+
+        //Automated PathFollowing stuff!
+        if (gamepad1.aWasPressed()) {
+            follower.followPath(pathChain.get());
+            automatedDrive = true;
+        }
+
+        //Stop automated following if the follower is done
+        if (automatedDrive && (gamepad1.xWasPressed() || !follower.isBusy())) {
+            follower.startTeleopDrive();
+            automatedDrive = false;
+        }
+
+        //Slow Mode
+        if (gamepad1.bWasPressed()) {
+            slowMode = !slowMode;
+        }
+
+        if (gamepad1.yWasPressed()) {
+            rt.setPower(topPower);
+        } else {
+            rt.setPower(0);
+        }
+
+        if (gamepad1.yWasPressed()) {
+            lt.setPower(topPower);
+        } else {
+            lt.setPower(0);
+        }
+
+        if (gamepad1.dpad_up) {
+            intake.setPower(intakePower);
+        }
+
+
+        if (gamepad1.dpad_down) {
+            intake.setPower(-intakePower);
+        }
+
+        if (gamepad1.dpadUpWasReleased() || gamepad1.dpadDownWasReleased()) {
+            intake.setPower(0);
+        }
+
+        telemetryM.debug("position", follower.getPose());
+        telemetryM.debug("velocity", follower.getVelocity());
+        telemetryM.debug("automatedDrive", automatedDrive);
+
+        telemetry.addData("Slow Mode: ", slowMode);
+        telemetry.addData("Starting Coordinates", startingPose);
+        telemetry.addData("Returning to base! ", automatedDrive);
+    }
+}
